256a548e0573aa60d12c1e724107254d495d5718
PabloGitu <36865553+PabloGitu@users.noreply.github.com>
2018-03-13 18:11:12 +0100

    Añadida la lista de participantes (solo vista).

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {ParticipantsPageComponent} from './componentes/participants-page/participants-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  {path: 'participants', component: ParticipantsPageComponent},
+  {path: '**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
