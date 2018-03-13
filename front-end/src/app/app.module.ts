76a0131a7eaaa5841f906b606194554ec7e0d0ad
PabloGitu <36865553+PabloGitu@users.noreply.github.com>
2018-03-13 17:18:59 +0100

    Añadida barra de navegación.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ParticipantsPageComponent } from './componentes/participants-page/participants-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParticipantsPageComponent,
+    NavbarComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/componentes/participants-page/participants-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'participants-page',
+  templateUrl: './participants-page.component.html',
+  styleUrls: ['./participants-page.component.scss']
+})
+export class ParticipantsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
