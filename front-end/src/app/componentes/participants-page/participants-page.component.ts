0a7c4039beac46154832ebb681e43d1d20eb6e55
DaniMartinez <djmartinezsuarez91@gmail.com>
2018-03-20 11:40:06 +0100

    Catch a lista de participantes en blanco.

--- front-end/src/app/componentes/participants-page/participants-page.component.ts
+++ front-end/src/app/componentes/participants-page/participants-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'participants-page',
@@ -10,16 +11,24 @@ import { User } from '../models/user.model';
 export class ParticipantsPageComponent implements OnInit {
 
   users: Array<User>;
+  errorListUsers:boolean = false;
 
   constructor(private userService: UserService) {
 
   }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe(data => {
-      console.log(data);
-      this.users = data;
-    });
-  };
+    this.userService.getUsers().subscribe(
+      data => 
+      {
+        console.log(data);
+        this.users = data;
+      },
+      error => {
+        this.errorListUsers = true;
+      }
+    );
+
+  } 
 
 }
