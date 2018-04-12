d7835417acbd121904da8ba8f780c384dcd332f8
Manu <manuamores1212@gmail.com>
2018-04-12 18:03:43 +0200

    cambios en voteService

--- back-end/src/main/java/drafter/repositories/VoteRepository.java
+++ back-end/src/main/java/drafter/repositories/VoteRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package drafter.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,15 +10,4 @@ import drafter.domain.Vote;
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
 	@Query("select v from Vote v where v.participant.id = ?1 and v.idea.id = ?2")
     Vote findByParticipantAndIdea(int paricipantId,int Ideaid);	
-=======
-package drafter.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import drafter.domain.Vote;
-
-@Repository
-public interface VoteRepository extends JpaRepository<Vote, Integer> {
->>>>>>> b06f537888ac9fe19da286e3fe1ae7c95ce431c4
 }
--- back-end/src/main/java/drafter/services/VoteService.java
+++ back-end/src/main/java/drafter/services/VoteService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package drafter.services;
 
 import java.util.List;
@@ -63,61 +62,3 @@ public class VoteService {
 		}
 	}
 }
-=======
-package drafter.services;
-
-import java.util.List;
-
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import drafter.domain.Vote;
-import drafter.repositories.VoteRepository;
-
-@Service
-@Transactional
-public class VoteService {
-	// Repository-------------------------------------------------------------------------------
-
-	@Autowired
-	private VoteRepository voteRepository;
-
-	// Constructor------------------------------------------------------------------------------
-
-	public VoteService() {
-			super();
-		}
-
-	// CRUD Methods------------------------------------------------------------------------------
-
-	public Vote create(Vote vote) {
-		return voteRepository.save(vote);
-	}
-
-	public Vote delete(int id) {
-		Vote vote = findById(id);
-		if (vote != null) {
-			voteRepository.delete(vote);
-		}
-		return vote;
-	}
-
-	public List<Vote> findAll() {
-		return voteRepository.findAll();
-	}
-
-	public Vote findById(Integer id) {
-		return voteRepository.findById(id).orElse(null);
-	}
-
-	public Vote update(Vote vote) {
-		return null;
-	}
-	
-	public Vote save(Vote vote) {
-		return voteRepository.save(vote);
-	}
-}
->>>>>>> b06f537888ac9fe19da286e3fe1ae7c95ce431c4
