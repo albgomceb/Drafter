aa61e8ae5eabebe8bfba82c9c90d4700ac938790
Manu <manuamores1212@gmail.com>
2018-04-26 17:22:38 +0200

    cambio jspdf y html2canvas

--- front-end/src/app/componentes/meetings/brainstorming/minutes-page/minutes-page.component.ts
+++ front-end/src/app/componentes/meetings/brainstorming/minutes-page/minutes-page.component.ts
@@ -43,29 +43,62 @@ export class BrainStormingMinutesPageComponent implements OnInit {
 
   downloadPDF() {
 
-    /*    let doc = new jsPDF();
-    
-        let specialElementHandlers = {
-          '#editor': function (element, renderer) {
-            return true;
-          }
-        }
-    */
+
     let content = this.content.nativeElement;
 
-    /*    doc.fromHTML(content.innerHTML, 15, 15,
-          {
-            'width': 100,
-            'elementHandlers': specialElementHandlers
-          },function(){doc.save('MeetingPDF');});
-    */
+    /*var canvasToImage = function (canvas) {
+      var img = new Image();
+      var dataURL = canvas.toDataURL('image/png');
+      img.src = dataURL;
+      return img;
+    };
+    var canvasShiftImage = function (oldCanvas, shiftAmt) {
+      shiftAmt = parseInt(shiftAmt) || 0;
+      if (!shiftAmt) { return oldCanvas; }
+
+      var newCanvas = document.createElement('canvas');
+      newCanvas.height = oldCanvas.height - shiftAmt;
+      newCanvas.width = oldCanvas.width;
+      var ctx = newCanvas.getContext('2d');
+
+      var img = canvasToImage(oldCanvas);
+      ctx.drawImage(img, 0, shiftAmt, img.width, img.height, 0, 0, img.width, img.height);
+
+      return newCanvas;
+    };
+
+    html2canvas(content).then(function (canvas) {
+      var pdf = new jsPDF('l', 'px'),
+        pdfInternals = pdf.internal,
+        pdfPageSize = pdfInternals.pageSize,
+        pdfScaleFactor = pdfInternals.scaleFactor,
+        pdfPageWidth = pdfPageSize.width,
+        pdfPageHeight = pdfPageSize.height,
+        totalPdfHeight = 0,
+        htmlPageHeight = canvas.height,
+        htmlScaleFactor = canvas.width / (pdfPageWidth * pdfScaleFactor),
+        safetyNet = 0;
+
+      while (totalPdfHeight < htmlPageHeight && safetyNet < 15) {
+        var newCanvas = canvasShiftImage(canvas, totalPdfHeight);
+        pdf.addImage(newCanvas, 'png', 0, 0, pdfPageWidth, 0, null, 'NONE');
+
+        totalPdfHeight += (pdfPageHeight * pdfScaleFactor * htmlScaleFactor);
+
+        if (totalPdfHeight < htmlPageHeight) {
+          pdf.addPage();
+        }
+        safetyNet++;
+      }
+
+      pdf.save('test.pdf');
+*/
     html2canvas(content).then(function (canvas) {
 
       var img = canvas.toDataURL("image/png");
       var doc = new jsPDF();
-      doc.addImage(img, 'JPEG',10,10,190,200);
+      doc.addImage(img, 'PNG',10,10,190,250);
       doc.save('Minutes.pdf');
     });
-
   }
 }
