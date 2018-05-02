78a9cd2dcc40875403b6c6e846c71603b3c470c0
crigalbla <cristian.gb.1995@hotmail.com>
2018-05-02 20:33:25 +0200

    Creada vista legal warning
    Issue: #92

---
+++ front-end/src/app/componentes/legal-page/legal-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'legal-page',
+  templateUrl: './legal-page.component.html',
+  styleUrls: ['./legal-page.component.scss']
+})
+export class LegalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
