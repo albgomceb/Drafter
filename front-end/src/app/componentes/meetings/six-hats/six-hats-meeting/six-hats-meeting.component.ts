4a7e22236be2d449c89206da76edc9d05d751c6e
Maite Jiménez <maitejr_95@hotmail.com>
2018-05-02 21:09:51 +0200

    Arreglado el error por el que se creaban conclusiones sin texto que se iban acumulando en front-end cada vez que se completada un ciclo de reasignación.

--- front-end/src/app/componentes/meetings/six-hats/six-hats-meeting/six-hats-meeting.component.ts
+++ front-end/src/app/componentes/meetings/six-hats/six-hats-meeting/six-hats-meeting.component.ts
@@ -85,7 +85,7 @@ export class SixHatsMeetingComponent implements OnInit {
         
         this.sortAttendants();
         this.getCurrentHat();
-        this.addFirstConclusion(); 
+        this.addConclusion(); 
 
         this.future = new Date().getTime() + this.sixHats.secondsLeft;
         this.$counter = Observable.interval(1000).map((x) => {        
@@ -102,7 +102,7 @@ export class SixHatsMeetingComponent implements OnInit {
         this.currentParticipant = participant;
         this.sortAttendants();
         this.getCurrentHat();
-        this.addFirstConclusion(); 
+        this.addConclusion(); 
       });      
     });
    
@@ -164,24 +164,12 @@ export class SixHatsMeetingComponent implements OnInit {
     }
   }
 
-  addFirstConclusion(){
-    var length = this.currentHat.hatConclusions.length;
-  
-    this.currentHat.hatConclusions[length] = new HatConclusion();
-    this.currentHat.hatConclusions[length].id = 0;
-    this.currentHat.hatConclusions[length].version = 0;
-    this.currentHat.hatConclusions[length].text = "";
-    this.currentHat.hatConclusions[length].isInput = true;
-    
-    
-  }
-
   // ----------------------- Métodos para añadir/eliminar una conclusión ----------------------
 
   addConclusion(){
     var i=0;                            
     for(var con of this.currentHat.hatConclusions) {
-      if(!con.text || con.text.trim().length == 0)
+      if(!this.checkNotBlank(con.text))
         this.currentHat.hatConclusions.splice(i, 1);
       
       i++;
@@ -209,13 +197,17 @@ export class SixHatsMeetingComponent implements OnInit {
     //Si la conclusion es un texto, se convierte en input
     } else if(!conclusion.isInput) {
       conclusion.isInput = true;
-      if(conclusion.text.trim() == 0)
-        this.deleteConclusion(conclusion.id, conclusionIndex);
-        
+    }
+    else if(!this.checkNotBlank(conclusion.text) && conclusion.isInput){
+      this.currentHat.hatConclusions.splice(conclusionIndex, 1);
     }
   }
 
   deleteConclusion(conclusionId : number, conclusionIndex : number) {
+    if(conclusionId === 0){
+      this.currentHat.hatConclusions.splice(conclusionIndex, 1);
+    }
+
     this.realTimeService.send('/sixHats/delete/' + conclusionId + '/', 
                                     WSResponseType.POP, 
                                     'hat-'+this.currentHat.color,
