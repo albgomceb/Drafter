da39eb1581f9d68bab330b56934ddf5dd549ad16
Marcos <marcos.kafok@gmail.com>
2018-05-10 19:10:24 +0200

    Optimizada subida de imagenes grandes al servidor

--- front-end/src/app/componentes/edit-profile-page/edit-profile-page.component.ts
+++ front-end/src/app/componentes/edit-profile-page/edit-profile-page.component.ts
@@ -14,33 +14,33 @@ import * as $ from 'jquery';
 })
 export class EditProfilePageComponent implements OnInit {
 
-  public user:User;
-  
-  public profileForm:FormGroup;
-  showError:Boolean = false;
-  errorNumber:Boolean = false;
-  name:string;
-  surname:string;
-  username:string;
-  phone:string;
-  email:string;
-  password:string;
+  public user: User;
 
-  errorListUsers:boolean = false;
+  public profileForm: FormGroup;
+  showError: Boolean = false;
+  errorNumber: Boolean = false;
+  name: string;
+  surname: string;
+  username: string;
+  phone: string;
+  email: string;
+  password: string;
 
-  constructor(private fb:FormBuilder, private profileService: ProfileService, private router: Router, private loginService: LoginService) {
+  errorListUsers: boolean = false;
+
+  constructor(private fb: FormBuilder, private profileService: ProfileService, private router: Router, private loginService: LoginService) {
     this.profileForm = this.fb.group({  // Esto es la validación de los campos
-      name: ['', ],
-      surname: ['',  ],
-      username: ['', ],
-      phone: ['', ],
-      email: ['', ],
-      password: ['', ],
-      photo: ['', ]
+      name: ['',],
+      surname: ['',],
+      username: ['',],
+      phone: ['',],
+      email: ['',],
+      password: ['',],
+      photo: ['',]
     })
 
 
-   }
+  }
 
   //  Validators.compose([Validators.required, Validators.minLength(5)]) 
 
@@ -48,27 +48,27 @@ export class EditProfilePageComponent implements OnInit {
     this.profileService.getUser().subscribe(
       data => {
         this.user = data;
-        if(this.user.photo == "/assets/img/none.png"){
+        if (this.user.photo == "/assets/img/none.png") {
           this.profileForm = this.fb.group({  // Esto es la validación de los campos
-            name: [this.user.name, Validators.compose([Validators.required]) ],
-            surname: [this.user.surname, Validators.compose([Validators.required]) ],
-            username: [this.user.username, Validators.compose([Validators.required, Validators.minLength(5)]) ],
-            phone: [this.user.phone, Validators.compose([Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('[0-9]+')]) ],
-            email: [this.user.email, Validators.compose([Validators.email]) ],
-            password: [this.user.password, Validators.compose([Validators.minLength(5)]) ],
-            photo: ['', Validators.compose([Validators.pattern('https?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}')  ]) ]
+            name: [this.user.name, Validators.compose([Validators.required])],
+            surname: [this.user.surname, Validators.compose([Validators.required])],
+            username: [this.user.username, Validators.compose([Validators.required, Validators.minLength(5)])],
+            phone: [this.user.phone, Validators.compose([Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('[0-9]+')])],
+            email: [this.user.email, Validators.compose([Validators.email])],
+            password: [this.user.password, Validators.compose([Validators.minLength(5)])],
+            photo: ['', Validators.compose([Validators.pattern('https?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}')])]
           })
         }
-        
-        else{
+
+        else {
           this.profileForm = this.fb.group({  // Esto es la validación de los campos
-            name: [this.user.name, Validators.compose([Validators.required]) ],
-            surname: [this.user.surname, Validators.compose([Validators.required]) ],
-            username: [this.user.username, Validators.compose([Validators.required, Validators.minLength(5)]) ],
-            phone: [this.user.phone, Validators.compose([Validators.required,Validators.minLength(9), Validators.maxLength(9), Validators.pattern('[0-9]+')]) ],
-            email: [this.user.email, Validators.compose([Validators.email]) ],
-            password: [this.user.password, Validators.compose([Validators.minLength(5)]) ],
-            photo: [this.user.photo, Validators.compose([Validators.pattern('https?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}')  ]) ]
+            name: [this.user.name, Validators.compose([Validators.required])],
+            surname: [this.user.surname, Validators.compose([Validators.required])],
+            username: [this.user.username, Validators.compose([Validators.required, Validators.minLength(5)])],
+            phone: [this.user.phone, Validators.compose([Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('[0-9]+')])],
+            email: [this.user.email, Validators.compose([Validators.email])],
+            password: [this.user.password, Validators.compose([Validators.minLength(5)])],
+            photo: [this.user.photo, Validators.compose([Validators.pattern('https?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}')])]
           })
         }
       },
@@ -76,10 +76,10 @@ export class EditProfilePageComponent implements OnInit {
         this.errorListUsers = true;
       }
     );
-   
+
   }
 
-  onSubmit(profileForm){
+  onSubmit(profileForm) {
     this.user.name = this.profileForm.value.name;
     this.user.surname = this.profileForm.value.surname;
     this.user.username = this.profileForm.value.username;
@@ -87,15 +87,15 @@ export class EditProfilePageComponent implements OnInit {
     this.user.email = this.profileForm.value.email;
     this.user.password = this.profileForm.value.password;
 
-   if((this.user.phone+'').length>=10 || (this.user.phone+'').length<9){
+    if ((this.user.phone + '').length >= 10 || (this.user.phone + '').length < 9) {
       this.errorNumber = true;
     }
-    else{
-      if(this.user.phone != ""){
-        this.profileService.updateUser(this.user).subscribe((res:any) =>{
+    else {
+      if (this.user.phone != "") {
+        this.profileService.updateUser(this.user).subscribe((res: any) => {
           this.router.navigate(['/me/']);
         }, error => {
-            this.showError=true;
+          this.showError = true;
         });
       }
     }
@@ -106,8 +106,12 @@ export class EditProfilePageComponent implements OnInit {
     var text = elem.text();
     elem.text("Uploading...");
 
+    this.resizeAndUpload(event.target.files[0], elem, text);
+  }
+
+  private upload(file, elem, text) {
     var formData = new FormData();
-    formData.append('file', event.target.files[0]);
+    formData.append('file', file);
     $.ajax({
       url: environment.baseApi + '/image/save',
       data: formData,
@@ -120,12 +124,51 @@ export class EditProfilePageComponent implements OnInit {
         this.user.photo = '';
         this.loginService.getPrincipal().photo = '';
         elem.text(text);
-        setTimeout(() => { 
+        setTimeout(() => {
           this.user.photo = '/data/image/' + this.user.id;
-          this.loginService.getPrincipal().photo = '/data/image/' + this.user.id; 
+          this.loginService.getPrincipal().photo = '/data/image/' + this.user.id;
         }, 0);
       }
     });
+  }
+
+  private resizeAndUpload(file, elem, text) {
+    var reader = new FileReader();
+    reader.onloadend = () => {
+
+      var tempImg = new Image();
+      tempImg.src = reader.result;
+      tempImg.onload = () => {
+        var tempW = tempImg.width;
+        var tempH = tempImg.height;
+
+        var canvas = document.createElement('canvas');
+        canvas.width = 128;
+        canvas.height = 128;
+        var ctx = canvas.getContext("2d");
+
+        if(tempW > tempH)
+          ctx.drawImage(tempImg, tempW/2-tempH/2, 0, tempH, tempH, 0, 0, 128, 128);
+        else
+          ctx.drawImage(tempImg, 0, tempH/2-tempW/2, tempW, tempW, 0, 0, 128, 128);
+
+        var dataURL = canvas.toDataURL("image/png");
+        console.log(dataURL);
+
+        this.upload(file, elem, text);
+      }
+
+    }
+    reader.readAsDataURL(file);
+  }
+
+  private dataURItoBlob(dataURI) {
+    var blobBin = atob(dataURI.split(',')[1]);
+    var array = [];
+    for (var i = 0; i < blobBin.length; i++) {
+      array.push(blobBin.charCodeAt(i));
+    }
+    var file = new Blob([new Uint8Array(array)], { type: 'image/png' });
   }
 
 }
